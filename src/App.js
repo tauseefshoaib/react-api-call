6b7d9d062184c4471b6a003ba657a876d7958fb6
tauseefshoaib <tauseefshoaib@gmail.com>
2023-12-11 19:43:05 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => setData(json));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <h1 className="header-text">Products</h1>
+      </div>
+      <div className="body">
+        {data.map((item, index) => {
+          return (
+            <div className="product-card">
+              <img
+                style={{ width: "100%" }}
+                src={item.image}
+                alt="product image"
+              />
+              <p1>{item.title}</p1>
+              <p1>
+                {item.description.length < 30
+                  ? item.description
+                  : item.description.slice(0, 30) + "..."}
+              </p1>
+              <h4>₹ {item.price}</h4>
+              <h3 style={{ color: "#640d14" }}>Rating : {item.rating.rate}</h3>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
