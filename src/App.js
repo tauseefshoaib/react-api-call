d30161c6366eb7ea60eae095eea0f824efb4136d
tauseefshoaib <tauseefshoaib@gmail.com>
2023-12-13 20:57:00 +0530

    search feature

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-/* eslint-disable jsx-a11y/img-redundant-alt */
 import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [data, setData] = useState([]);
+  const [filterData, setFilterData] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  function onSearchInput(value) {
+    setSearchText(value);
+    let searchInput = value.toLowerCase();
+    let result = data.filter((item) =>
+      item.title.toLowerCase().includes(searchInput)
+    );
+    setFilterData(result);
+  }
+
+  const renderStars = (rating) => {
+    const stars = [];
+    const numberOfStars = Math.floor(rating);
+    for (let i = 0; i < numberOfStars; i++) {
+      stars.push(<span style={{ color: "#640d14" }}>★</span>);
+    }
+    return stars;
+  };
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
-      .then((json) => setData(json));
+      .then((data) => {
+        setData(data);
+        setFilterData(data);
+      });
   }, []);
 
   return (
@@ -16,26 +38,36 @@ function App() {
       <div className="header">
         <h1 className="header-text">Products</h1>
       </div>
+      <input
+        className="search-input"
+        placeholder="search..."
+        value={searchText}
+        onChange={(e) => onSearchInput(e.target.value)}
+      />
       <div className="body">
-        {data.map((item, index) => {
-          return (
-            <div className="product-card">
-              <img
-                style={{ width: "100%" }}
-                src={item.image}
-                alt="product image"
-              />
-              <p1>{item.title}</p1>
-              <p1>
-                {item.description.length < 30
-                  ? item.description
-                  : item.description.slice(0, 30) + "..."}
-              </p1>
-              <h4>₹ {item.price}</h4>
-              <h3 style={{ color: "#640d14" }}>Rating : {item.rating.rate}</h3>
-            </div>
-          );
-        })}
+        {filterData.length > 0 ? (
+          filterData.map((item) => {
+            return (
+              <div className="product-card" key={item.id}>
+                <img
+                  style={{ width: "100%", height: "200px" }}
+                  src={item.image}
+                  alt={item.title}
+                />
+                <h4>{item.title}</h4>
+                <h3>₹ {item.price}</h3>
+                <p1>
+                  {item.description.length < 30
+                    ? item.description
+                    : item.description.slice(0, 30) + "..."}
+                </p1>
+                <h4>Rating: {renderStars(item.rating.rate)}</h4>
+              </div>
+            );
+          })
+        ) : (
+          <h1>Loading...</h1>
+        )}
       </div>
     </div>
   );
